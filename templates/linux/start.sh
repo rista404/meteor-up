8dd42f21081c903ec0bf7f03e75314a4a4a42f72
Sergio Alexander <xergioalex@gmail.com>
2016-08-02 11:58:08 -0500

    Update docker container with gm included

--- templates/linux/start.sh
+++ templates/linux/start.sh
@@ -15,10 +15,10 @@ docker rm -f $APPNAME-frontend
 
 # We don't need to fail the deployment because of a docker hub downtime
 set +e
-# docker pull meteorhacks/meteord:base
+# docker pull ianmartorell/meteord-graphicsmagick:base
 set -e
-docker build -t meteorhacks/meteord:app - << EOF
-FROM meteorhacks/meteord:base
+docker build -t ianmartorell/meteord-graphicsmagick:app - << EOF
+FROM ianmartorell/meteord-graphicsmagick:base
 RUN apt-get install graphicsmagick -y
 EOF
 
@@ -34,7 +34,7 @@ if [ "$USE_LOCAL_MONGO" == "1" ]; then
     --hostname="$HOSTNAME-$APPNAME" \
     --env=MONGO_URL=mongodb://mongodb:27017/$APPNAME \
     --name=$APPNAME \
-    meteorhacks/meteord:app
+    ianmartorell/meteord-graphicsmagick:app
 else
   docker run \
     -d \
@@ -44,7 +44,7 @@ else
     --hostname="$HOSTNAME-$APPNAME" \
     --env-file=$ENV_FILE \
     --name=$APPNAME \
-    meteorhacks/meteord:app
+    ianmartorell/meteord-graphicsmagick:app
 fi
 
 <% if(typeof sslConfig === "object")  { %>
